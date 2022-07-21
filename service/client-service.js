52435fb175904da62e2a2d7616e34e0a9b367524
yagamiar15 <dario11elme@hotmail.com>
2022-07-21 17:02:02 -0300

    agregados controladores, agregado fx de registro a la BD

--- service/client-service.js
+++ service/client-service.js
@@ -1,62 +1,19 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement('tr')
-    const contenido = `
-            <td class="td" data-td>
-            ${nombre}
-            </td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>
-        `;
-        linea.innerHTML = contenido;
-        return linea
-};
-
-const table = document.querySelector("[data-table]")
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 
-const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-        http.open("GET", "http://localhost:3000/perfil");
-        
-        http.send();
-        
-        http.onload = () => {
-            const response = JSON.parse(http.response);
-            if(http.status >= 400){
-                reject(response)              
-            } else { 
-                resolve(response)
-            }
-        };   
+const crearCliente = (nombre, email) => {
+    return fetch("http://localhost:3000/perfil", { 
+        method: "POST",
+        headers: {
+            "content-type": "application/json"   
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
     });
-
-    return promise
+    
 };
 
-listaClientes().then((data) => {
-    data.forEach( perfil => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-    table.appendChild(nuevaLinea);
- });
-}).catch((error) => alert("Hubo un error"));
 
-
+export const clientServices = {
+    listaClientes,
+    crearCliente
+};
